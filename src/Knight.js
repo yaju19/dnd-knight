c76acf50a7f8e88e592f44cd0e518dfb20013b4e
yaju19 <yajuvendra.rathor@teachedison.com>
2022-06-20 22:04:30 +0530

    first commit

---
+++ src/Knight.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ItemTypes } from "./Constants";
+import { useDrag } from "react-dnd";
+
+export default function Knight() {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.KNIGHT,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 48,
+        fontWeight: "bold",
+        cursor: "move",
+      }}
+    >
+      ♘
+    </div>
+  );
+}
